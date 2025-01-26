592c5a9b7c74f6f229227a688a7f547cdc3d5649
Rowland <row2k@jormungandr.lan>
2025-01-25 22:26:46 -0500

    correcting URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://starboardside.github.io/docs',
+  url: 'https://starboardside.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
