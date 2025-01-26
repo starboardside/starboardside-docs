3a972466080d8b69c8826765131903aa3417b42b
row2k <rowlandrzhang@gmail.com>
2025-01-25 22:52:32 -0500

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://starboardside.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/starboardside-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'starboardside', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'starboardside-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: true,
 
